162d6e073fe17067c8f715fc4d68d08468b308bb
Paolo Tranquilli <redsun82@gmail.com>
2025-07-01 06:37:32 +0200

    Use pre-commit action in CI

--- .github/workflows/check.py
+++ .github/workflows/check.py
@@ -6,6 +6,4 @@ def check():
     on.pull_request().push()
     uses("actions/checkout@v4")
     uses("astral-sh/setup-uv@v5")
-    step("Check formatting").run("uv run black --check .")
-    step("Run tests").if_(~cancelled()).run("uv run pytest")
-    step("Check generation").if_(~cancelled()).run("uv run gh-gen --check")
+    step("Check").uses("pre-commit/action@v3.0.1")
